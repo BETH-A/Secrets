b8a937fbc3499684d415f62258f1ecd61148a6c1
Beth-A <elizabeth78@me.com>
2020-05-15 11:26:40 -0400

    Changed security from mongoose-encryption to hashing

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 ///Access NPMs///
 const app = express();
@@ -27,7 +27,6 @@ const userSchema = new mongoose.Schema ({
   password: String,
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -42,7 +41,7 @@ app.route("/login")
 })
 .post(function(req, res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, function(err, foundUser){
     if (err) {
@@ -64,7 +63,7 @@ app.route("/register")
 .post(function(req, res){
   const newUser = new User ({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function(err){
