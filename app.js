9eace750dd52bbe124fff97b0e1bb8baf3dd5ef6
Beth-A <elizabeth78@me.com>
2020-05-19 13:20:59 -0400

    Secure MongoDB credentials to get MongoDB Atlas to work

--- app.js
+++ app.js
@@ -75,8 +75,8 @@ passport.use(new FacebookStrategy({
 
 
 ///Connect to MongoDB via Mongoose///
-// mongoose.connect("mongodb+srv://" + userName + ":" + password +"@secrets0-jcgh2.mongodb.net/userDB", {
-mongoose.connect("mongodb+srv://admin-beth:ZPs4SnPS5o05KVdy@secrets0-jcgh2.mongodb.net/userDB", {
+mongoose.connect("mongodb+srv://" + userName + ":" + password +"@secrets0-jcgh2.mongodb.net/userDB", {
+// mongoose.connect("mongodb+srv://admin-beth:ZPs4SnPS5o05KVdy@secrets0-jcgh2.mongodb.net/userDB", {
 useUnifiedTopology: true,
 useNewUrlParser: true,
 });
